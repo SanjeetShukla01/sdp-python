ae978c142cd865de0c5275a1ebf38b1019336f83
SanjeetShukla01 <sanjeet.shukla089@gmail.com>
2024-04-14 13:38:16 +0530

    adding double linked list

--- data_structures/double_linked_list.py
+++ data_structures/double_linked_list.py
@@ -31,6 +31,42 @@ class DoublyLinkedList:
             self.head.prev = new_node
             self.head = new_node
 
+    def delete(self, data):
+        current_node = self.head
+        while current_node:
+            if current_node.data == data:
+                if current_node.prev:
+                    current_node.prev.next = current_node.next
+                else:
+                    self.head = current_node.next
+                if current_node.next:
+                    current_node.next.prev = current_node.prev
+                else:
+                    self.tail = current_node.prev
+                return
+            current_node = current_node.next
+
+    def print_forward(self):
+        current_node = self.head
+        while current_node:
+            print(current_node.data, end=" => ")
+            current_node = current_node.next
+        print(None)
+
+    def print_backward(self):
+        current_node = self.tail
+        while current_node:
+            print(current_node.data, end=" => ")
+            current_node = current_node.prev
+        print(None)
 
 
-
+if __name__ == "__main__":
+    dll = DoublyLinkedList()
+    dll.append(1)
+    dll.append(2)
+    dll.append(3)
+    dll.prepend(0)
+    dll.print_forward()  # Output: 0 -> 1 -> 2 -> 3 -> None
+    dll.delete(2)
+    dll.print_backward()  # Output: 3 -> 1 -> 0 -> None
