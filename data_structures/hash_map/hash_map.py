1e3f644a6c320b5fce0006e60083afbc935bc802
SanjeetShukla01 <sanjeet.shukla089@gmail.com>
2024-04-19 11:46:15 +0530

    adding hash function

--- data_structures/hash_map/hash_map.py
+++ data_structures/hash_map/hash_map.py
@@ -12,7 +12,20 @@ class HashTable:
         self.size = size
         self.table = [[] for _ in range(size)]
 
+    def _hash_function(self, key):
+        return key % self.size
+
+    def set(self, key, value):
+        hash_index = self._hash_function(key)
+        for item in self.table[hash_index]:
+            if item.key == key:
+                item.value = value
+                return
+        self.table[hash_index].append(Item(key, value))
+
 
 if __name__ == '__main__':
     table = [[] for _ in range(5)]
     print(table)
+    my_hash_map = HashTable(7)
+    print(my_hash_map._hash_function(3))
