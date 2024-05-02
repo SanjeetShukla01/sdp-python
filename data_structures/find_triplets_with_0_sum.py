0bc6af842ee756e8cfb5e67a9a58c5cd9725609b
SanjeetShukla01 <sanjeet.shukla089@gmail.com>
2024-05-02 11:29:34 +0530

    triplets in a list

---
+++ data_structures/find_triplets_with_0_sum.py
@@ -0,0 +1,17 @@
+# Given a list of integers, return elements a, b, c such that a + b + c = 0.
+#     Args:
+#         nums: list of integers
+#     Returns:
+#         list of lists of integers where sum(each_list) == 0
+#     Examples:
+#         >>> find_triplets_with_0_sum([-1, 0, 1, 2, -1, -4])
+#         [[-1, -1, 2], [-1, 0, 1]]
+#         >>> find_triplets_with_0_sum([])
+#         []
+#         >>> find_triplets_with_0_sum([0, 0, 0])
+#         [[0, 0, 0]]
+#         >>> find_triplets_with_0_sum([1, 2, 3, 0, -1, -2, -3])
+#         [[-3, 0, 3], [-3, 1, 2], [-2, -1, 3], [-2, 0, 2], [-1, 0, 1]]
+
+def triplets_with_zero_sum(arr: list) -> list:
+    list()
